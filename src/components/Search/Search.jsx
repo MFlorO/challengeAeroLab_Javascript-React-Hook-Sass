2e8c701faafc2ac2e9e092f096b51d0590cc4c19
Flori O <floroldani@hotmail.com>
2022-11-20 23:09:03 -0300

    agregar trim para validar los espacios de los costados

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -12,6 +12,7 @@ const Search = ({productCopia, setProductCopia, productAll, setCurrentPage}) => 
     setSearch((event.target.value).toLowerCase())
   };
 
+  console.log(search)
 
   const filtrar = (search) => {
 
@@ -32,7 +33,7 @@ const Search = ({productCopia, setProductCopia, productAll, setCurrentPage}) => 
 
   const handleSubmit = (event) => {
     event.preventDefault(); //para que no refresque al dar "buscar"
-	  filtrar(search); //le paso lo que escribo en el input a la funcion filtrar. Esta va a recorrer el array con los datos de productos  y los va a filtrar de acuerdo al dato que escriba en el input
+	  filtrar(search.trim()); //le paso lo que escribo en el input a la funcion filtrar. Esta va a recorrer el array con los datos de productos  y los va a filtrar de acuerdo al dato que escriba en el input
     setSearch("");
     setCurrentPage(0) //Reseteo la pagina en cero. Caso: Cuando hago una busqueda, tiene que funcionar el paginado
   };
